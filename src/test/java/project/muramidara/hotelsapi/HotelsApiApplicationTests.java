c04c5f68620b8e96989c6743c66adfeba86a6ae7
Muramidara <muramidara@gmail.com>
2026-07-24 14:17:57 +0300

    First commit

---
+++ src/test/java/project/muramidara/hotelsapi/HotelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.muramidara.hotelsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
