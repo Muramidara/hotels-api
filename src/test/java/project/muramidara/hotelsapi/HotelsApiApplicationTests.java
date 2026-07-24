85384b37cd14cd2b50a6bacc137bdd35406015dd
Muramidara <muramidara@gmail.com>
2026-07-24 21:07:21 +0300

    Created method to test HotelRepository in HotelRepositoryTest

--- src/test/java/project/muramidara/hotelsapi/HotelsApiApplicationTests.java
+++ src/test/java/project/muramidara/hotelsapi/HotelsApiApplicationTests.java
@@ -2,8 +2,10 @@ package project.muramidara.hotelsapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 
 @SpringBootTest
+@TestConfiguration
 class HotelsApiApplicationTests {
 
     @Test
