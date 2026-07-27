06689abc8acd3fd63f2e2be6a1571ab5c3d96f47
Muramidara <muramidara@gmail.com>
2026-07-27 10:42:37 +0300

    Added test for create() method

--- src/test/java/project/muramidara/hotelsapi/repository/HotelResporitoryTests.java
+++ src/test/java/project/muramidara/hotelsapi/repository/HotelResporitoryTests.java
@@ -1,9 +1,14 @@
 package project.muramidara.hotelsapi.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import project.muramidara.hotelsapi.database.entity.Hotel;
 import project.muramidara.hotelsapi.database.repository.HotelRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class HotelResporitoryTests {
@@ -13,5 +18,21 @@ public class HotelResporitoryTests {
     @BeforeEach
     public void setUp(){
         hotelRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Test save hotel functionality")
+    public void givenHotelObject_whenSave_thenHotelIsCreated(){
+        //given
+        Hotel hotelToSave = Hotel.builder()
+                .name("test hotel")
+                .brand("test brand")
+                .description("description for test hotel")
+                .build();
+        //when
+        Hotel savedHotel = hotelRepository.save(hotelToSave);
+        //then
+        assertThat(savedHotel).isNotNull();
+        assertThat(savedHotel.getId()).isNotNull();
     }
 }
